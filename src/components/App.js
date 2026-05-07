89b76f8f9152efc557fdd081abe7dc7cbfb9333e
Archit <archit483@gmail.com>
2026-05-07 14:23:02 +0530

    'tooltip'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
 import React from "react";
-import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ padding: "100px" }}>
+      
+      <Tooltip text="This is a tooltip">
+        <h1>Hover over me</h1>
+      </Tooltip>
+
+      <hr />
+
+      <Tooltip text="This is another tooltip">
+        <p>Hover over me to see another tooltip</p>
+      </Tooltip>
+
+      <hr />
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Tooltip.js
@@ -0,0 +1,39 @@
+// Tooltip.js
+
+import React, { useState } from "react";
+
+function Tooltip({ text, children }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={()=>setIsVisible(true)}
+      onMouseLeave={()=>setIsVisible(false)}
+      style={{ position: "relative", display: "inline-block" }}
+    >
+      {children}
+
+      {isVisible && (
+        <span
+          className="tooltiptext"
+          style={{
+            position: "absolute",
+            bottom: "125%",
+            left: "50%",
+            transform: "translateX(-50%)",
+            backgroundColor: "#e91c1c",
+            color: "#fff",
+            padding: "8px 12px",
+            borderRadius: "5px",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {text}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default Tooltip;
